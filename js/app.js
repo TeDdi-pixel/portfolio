d956b3940af2ac1082476513a1028129d0aed9fb
TeDdi-pixel <71564400+TeDdi-pixel@users.noreply.github.com>
2022-06-21 10:52:41 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+new Swiper('.image-slider',{
+    navigation: {
+        nextEL: '.swiper-button-next',
+        prevEL: '.swiper-button-prev'
+    },
+    pagination:{
+        el: 'swiper-pagination',
+        clickable:true,
+    },
+});    
