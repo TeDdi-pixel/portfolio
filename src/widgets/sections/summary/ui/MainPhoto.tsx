0389a6192d9aac7078fbdcc4f62d3a0001623bd2
TeDdi-pixel <2003valuyga2003@gmail.com>
2024-12-12 01:49:25 +0200

    main photo border radius changed

--- src/widgets/sections/summary/ui/MainPhoto.tsx
+++ src/widgets/sections/summary/ui/MainPhoto.tsx
@@ -4,8 +4,8 @@ const MainPhoto = () => {
   return (
     <m.div
       // {...imgWrapperConfig}
-      {...defaultAnimation({direction: 20})}
-      className="mx-auto bp912:mx-0 w-[280px] h-[280px] xl:w-[480px] xl:h-[430px] rounded-full bp912:rounded-full overflow-hidden shadow-custom2"
+      {...defaultAnimation({ direction: 20 })}
+      className="mx-auto bp912:mx-0 w-[280px] h-[280px] xl:w-[480px] xl:h-[430px] rounded-full bp912:rounded-3xl overflow-hidden shadow-custom2"
     >
       <img
         src="./main_photo.webp"
